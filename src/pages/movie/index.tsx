19175928c966fcadcd0629ca27a74915d352319c
Eric Muradyan <ermanfantasy@gmail.com>
2024-03-21 23:35:19 +0400

    Add: movie details page initial UI

--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -5,6 +5,7 @@ import Api from "../../api";
 // types
 import { MovieDetails } from "../../types/movies";
 import { useSnackbar } from "notistack";
+import { FaCalendar, FaClock, FaStar } from "react-icons/fa";
 
 export default function MoviePage() {
   const { id } = useParams();
@@ -33,5 +34,33 @@ export default function MoviePage() {
     getMovie();
   }, [getMovie]);
 
-  return <div>{loading ? "Loading..." : JSON.stringify(movie, null, 2)}</div>;
+  return (
+    <section className="py-10">
+      <div className="max-width-limit h-auto m-auto gap-5 flex flex-col sm:flex-row sm:space-x-10 text-gray-300">
+        <div className="w-full sm:w-[80%] flex flex-col space-y-5">
+          <h1 className="text-4xl text-sky-500 drop-shadow-[4px_4px_4px_rgba(12,12,12,0.5)]">{movie?.title}</h1>
+          <div className="flex space-x-4">
+            <FaCalendar className="text-sky-500 text-[18px]" />
+            <span>{movie?.release_date.split("-").reverse().join(".")}</span>
+          </div>
+          <div className="flex space-x-4">
+            <FaClock className="text-sky-500 text-[20px]" />
+            <p>{movie?.runtime} min.</p>
+          </div>
+          <div className="flex space-x-4">
+            <FaStar className="text-sky-500 text-[22px]" />
+            <p>{movie?.vote_average.toFixed(1)}</p>
+          </div>
+          <p className="text-justify">{movie?.overview}</p>
+        </div>
+        <div className="w-full sm:w-[20%] flex justify-center sm:block -order-1 sm:order-1">
+          <img
+            className="w-[65%] sm:w-full rounded-md"
+            src={process.env.REACT_APP_IMAGE_URL_PREFIX + movie?.poster_path}
+            alt={movie?.title}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
