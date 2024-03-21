ff7b560e62d8f5c6e1956897f0fd4efaf15ba431
Eric Muradyan <ermanfantasy@gmail.com>
2024-03-22 00:32:25 +0400

    Fix: auto scrolling issue

--- src/components/movies/card/index.tsx
+++ src/components/movies/card/index.tsx
@@ -12,29 +12,29 @@ interface Props {
 
 function MovieCard({ movie }: Props) {
   return (
-    <div className="w-[250px] sm:w-[200px] h-[330px] m-auto relative p-3 border-2 border-solid border-sky-700 rounded-md flex flex-col space-y-2 duration-700 hover:shadow-gray-300 hover:shadow-xl hover:border-sky-900">
-      <div className="h-[230px]">
-        <Image className="w-full h-full rounded-md" src={movie.poster_path} alt={movie.title} />
-      </div>
-      <div className="h-[48px] overflow-hidden font-bold">
-        <Link to={`/movie/${movie.id}-${movie.title.toLowerCase().split(" ").join("-")}`}>
+    <Link to={`/movie/${movie.id}`}>
+      <div className="w-[250px] sm:w-[200px] h-[330px] m-auto relative p-3 border-2 border-solid border-sky-700 rounded-md flex flex-col space-y-2 duration-700 hover:shadow-gray-300 hover:shadow-xl hover:border-sky-900">
+        <div className="h-[230px]">
+          <Image className="w-full h-full rounded-md" src={movie.poster_path} alt={movie.title} />
+        </div>
+        <div className="h-[48px] overflow-hidden font-bold">
           <h4 className="duration-500 hover:text-sky-700">{movie.title}</h4>
-        </Link>
-      </div>
-      <div className="flex justify-between font-bold">
-        <div className="flex space-x-2">
-          <FaCalendar className="text-sky-700 text-[18px]" />
-          <span>{movie.release_date.slice(0, 4)}</span>
         </div>
-        <div className="flex space-x-2">
-          <FaStar className="text-sky-700 text-[20px]" />
-          <span>{movie.vote_average.toFixed(1)}</span>
+        <div className="flex justify-between font-bold">
+          <div className="flex space-x-2">
+            <FaCalendar className="text-sky-700 text-[18px]" />
+            <span>{movie.release_date.slice(0, 4)}</span>
+          </div>
+          <div className="flex space-x-2">
+            <FaStar className="text-sky-700 text-[20px]" />
+            <span>{movie.vote_average.toFixed(1)}</span>
+          </div>
         </div>
+        <button>
+          <FaHeart className={`absolute right-4 top-4 text-2xl duration-500 animate-pulse`} />
+        </button>
       </div>
-      <button>
-        <FaHeart className={`absolute right-4 top-4 text-2xl duration-500 animate-pulse`} />
-      </button>
-    </div>
+    </Link>
   );
 }
 
--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -38,6 +38,7 @@ export default function MoviePage() {
 
   useEffect(() => {
     getMovie();
+    window.scrollTo(0, 0);
   }, [getMovie]);
 
   return (
